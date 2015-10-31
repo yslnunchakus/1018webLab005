1acb58f2f2ca8d38b7bed3b5758ddbe9baa15436
姚晟隆 <491655067@qq.com>
2015-10-31 15:16:00 +0000

    Make a bsic User model(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true, 
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
-                        validates :password, presence: true, length: { minimum: 6 }
+                   validates :password, presence: true, length: { minimum: 6 }
       validates :password_confirmation, presence: true
       has_secure_password
 end
