603047d28184701aafa74a200a9f61f411a804be
姚晟隆 <491655067@qq.com>
2015-11-01 15:45:25 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
       end
 
   def new
+        @user = User.new
   end
+  def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+          flash[:success] = "Welcome to the Campus Swap App!"
+           redirect_to @user
+           # Handle a successful save.
+        else
+            render 'new'
+            # Handle an unsuccessful save.     
+        end
+      end
 end
