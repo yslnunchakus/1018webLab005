f998b4daa2b95e0983780dfb8ee2d124ea080cbc
姚晟隆 <491655067@qq.com>
2015-11-01 14:26:29 +0000

    Skeleton User profile page support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
-
+          resources :users
           root 'static_pages#home'
           get 'help'    => 'static_pages#help'
           get 'about'   => 'static_pages#about'
